eb974e35497b83e634d36ee156b7c622a75a3fd6
biggoron <dan.ringwald12@gmail.com>
2016-03-04 14:44:59 +0900

    removed some table deletion

--- db/migrate/20160210083634_create_articles.rb
+++ db/migrate/20160210083634_create_articles.rb
@@ -1,7 +1,5 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    drop_table :comments
-    drop_table :articles
     create_table :articles do |t|
       t.string :title
       t.integer :layout
