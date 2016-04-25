2563529156b06e543ea5dac675ad344a4dba955b
biggoron <dan.ringwald12@gmail.com>
2016-04-25 16:57:33 +0900

    corrected a deleting bug on writings

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,6 +1,6 @@
 class Writing < ActiveRecord::Base
-  belongs_to :article, dependent: :destroy
-  has_many :remarks
+  belongs_to :article
+  has_many :remarks, dependent: :destroy
   def diff
     return level_to_text(self.rel_lvl)
   end
