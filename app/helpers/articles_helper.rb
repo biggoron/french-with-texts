58b6db87ed64c9c695253dc028933d85f40d2db3
biggoron <dan.ringwald12@gmail.com>
2016-06-30 22:08:06 +0900

    Added images boxes helper and an example

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,20 @@
 module ArticlesHelper
+  def sub_img(image, &text)
+    img = image_tag(image, class: "illu-img")
+    html = <<-HTML
+      <div class="illu-article">
+      #{img}
+        <div class="illu-legend">
+          #{capture(&text)}
+        </div>
+      </div>
+    HTML
+    html.html_safe
+  end
+# def sub_img(img, &text)
+#   img = image_tag(img, class: "")
+#   content = &text
+#   content_d = div_tag()
+#   div_tag(img + content_d ...)
+# end
 end
